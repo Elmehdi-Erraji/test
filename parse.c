67dce1dbd31878291be9713b269801764c1acdac
Mehdi <elmahdi20erraji@gmail.com>
2023-06-20 19:43:51 +0100

    fix

--- parse.c
+++ parse.c
@@ -10,24 +10,24 @@
  */
 void parseline(line_t *line, char *buffer)
 {
-	unsigned int i;
-	char *token = NULL;
+    unsigned int i;
+    char *token = NULL;
 
-	line->content = malloc(sizeof(char *) * 3);
-	if (!line->content)
-	{
-		fprintf(stderr, "Error: malloc failed");
-		exit(EXIT_FAILURE);
-	}
+    line->content = malloc(sizeof(char *) * 3);
+    if (!line->content)
+    {
+        fprintf(stderr, "Error: malloc failed");
+        exit(EXIT_FAILURE);
+    }
 
-	token = strtok(buffer, " '\n'");
-	for (i = 0; token && i < 2; i++)
-	{
-		line->content[i] = token;
-		token = strtok(NULL, " \n");
-	}
+    token = strtok(buffer, " '\n'");
+    for (i = 0; token && i < 2; i++)
+    {
+        line->content[i] = token;
+        token = strtok(NULL, " \n");
+    }
 
-	line->content[i] = NULL;
+    line->content[i] = NULL;
 }
 
 /**
@@ -38,34 +38,40 @@ void parseline(line_t *line, char *buffer)
  */
 void parsefile(FILE *file)
 {
-	size_t size = 0;
-	meta_t *meta = NULL;
-	line_t line;
+    size_t size = 0;
+    meta_t *meta = NULL;
+    line_t line;
 
-	meta = malloc(sizeof(meta_t));
-	if (!meta)
-	{
-		fprintf(stderr, "Error: malloc failed");
-		exit(EXIT_FAILURE);
-	}
+    meta = malloc(sizeof(meta_t));
+    if (!meta)
+    {
+        fprintf(stderr, "Error: malloc failed");
+        exit(EXIT_FAILURE);
+    }
 
-	line.number = 0;
-	line.content = NULL;
+    line.number = 0;
+    line.content = NULL;
 
-	meta->file = file;
-	meta->stack = NULL;
-	meta->buf = NULL;
+    meta->file = file;
+    meta->stack = NULL;
+    meta->buf = malloc(sizeof(char) * MAX_LINE_SIZE);  // Add this line
 
-	while (getline(&(meta->buf), &size, meta->file) != -1)
-	{
-		line.number++;
-		parseline(&line, meta->buf);
-		if (line.content)
-			get_op_func(line, meta)(&(meta->stack), line.number);
-	}
+    if (!meta->buf)  // Add this condition
+    {
+        fprintf(stderr, "Error: malloc failed");
+        exit(EXIT_FAILURE);
+    }
 
-	free(meta->buf);
-	free_stack(&(meta->stack));
-	fclose(meta->file);
-	free(meta);
+    while (fgets(meta->buf, MAX_LINE_SIZE, meta->file) != NULL)  // Update this line
+    {
+        line.number++;
+        parseline(&line, meta->buf);
+        if (line.content)
+            get_op_func(line, meta)(&(meta->stack), line.number);
+    }
+
+    free(meta->buf);
+    free_stack(&(meta->stack));
+    fclose(meta->file);
+    free(meta);
 }
